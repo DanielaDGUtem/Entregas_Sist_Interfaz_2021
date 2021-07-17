5ffe054252e181a3d2882203660d720cc751f5ff
DanielaDGUtem <ddurang@utem.cl>
2021-07-16 20:42:53 -0400

    .

--- landing_page/assets/Js/modal.js
+++ landing_page/assets/Js/modal.js
@@ -1,6 +1,6 @@
 $('.gallery_img').click(function(e){
     var img = e.target.src;
-    var modal = 'div class="modal"><img class="modal_img" src=" '+ img +' "><div class="modal_boton" id="modal_botom">x</div></div>';
+    var modal = '<div class="modal" id="modal"><img class="modal_img" src=" '+ img +' "><div class="modal_boton" id="modal_boton">X</div></div>';
     $("body").append (modal);
     $('#modal_boton').click(function(){
       $('#modal').remove();
